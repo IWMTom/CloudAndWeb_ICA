5ca64b7be2813aec9b1c88e3b008741533d4bf4b
Tom Wilson <tom@iwm.pw>
2017-11-15 22:14:30 +0000

    Task 2 Complete

---
+++ Task2Start/Controllers/StaffController.cs
@@ -0,0 +1,52 @@
+﻿using HebbraCoDbfModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Task2Start.Models;
+
+namespace Task2Start.Controllers
+{
+    public class StaffController : ApiController
+    {
+        private HebbraCoDbfModel.HebbraCo16Model context = new HebbraCo16Model(); // A database context reference
+
+        // Called when a HTTP GET request comes in at /api/Staff/{staffCode}
+        [Route("api/Staff/{staffCode}")]
+        public StaffDetailDTO Get(string staffCode)
+        {
+            if (String.IsNullOrEmpty(staffCode))
+            {
+                throw new HttpException(400, "Bad Request"); // If an ID isn't provided in the URL, a HTTP 400 exception is thrown
+            }
+
+            var thisStaff = context.Staffs.FirstOrDefault(s => s.staffCode.Equals(staffCode, StringComparison.OrdinalIgnoreCase) && s.Active == true); // Gets the staff member where the code equals the ID from the URL, regardless of case, and isn't soft deleted - equals null if not found
+            if (thisStaff == null)
+            {
+                throw new HttpException(404, "Not Found"); // If the staff member doesn't exist for the given ID, a HTTP 404 exception is thrown
+            }
+            else
+            {
+                var dto = StaffDetailDTO.buildDTO(thisStaff); // Passes the collection to the Staff Data Transfer Object to be formatted
+                return dto;
+            }
+        }
+
+        // Called when a HTTP GET request comes in at /api/Staff//BusinessUnit/{buCode}
+        [Route("api/Staff/BusinessUnit/{buCode}")]
+        public IEnumerable<StaffDetailDTO> getStaffByBusinessUnit(string buCode)
+        {
+            if (String.IsNullOrEmpty(buCode))
+            {
+                throw new HttpException(400, "Bad Request"); // If an ID isn't provided in the URL, a HTTP 400 exception is thrown
+            }
+
+            var staff = context.Staffs.Where(s => s.Active == true && s.BusinessUnit.businessUnitCode.Equals(buCode, StringComparison.OrdinalIgnoreCase)); // Gets all staff from the database where the active flag is true and the business unit code matches as a collection of raw data
+            var dto = StaffDetailDTO.buildList(staff); // Passes the collection to the Staff Detail Data Transfer Object to be formatted
+            return dto; // Returns the fomatted data (as JSON or XML, depending on which is chosen)
+        }
+    }
+}
---
+++ Task2Start/Models/StaffDetailDTO.cs
@@ -0,0 +1,66 @@
+﻿using HebbraCoDbfModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task2Start.Models
+{
+    public class StaffDetailDTO
+    {
+
+        public string firstName { get; set; }
+
+        public string middleName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string dob { get; set; }
+
+        public string startDate { get; set; }
+
+        public string profile { get; set; }
+
+        public string emailAddress { get; set; }
+
+        public string staffCode { get; set; }
+
+        public string fullName { get; set; }
+
+        public static StaffDetailDTO buildDTO(Staff s)
+        {
+            StaffDetailDTO staffDTO = new Models.StaffDetailDTO()
+            {
+                staffCode = s.staffCode.Trim(),
+                fullName = s.firstName + " " + (s.middleName == null ? "" : (s.middleName + " ")) + s.lastName,
+                firstName = s.firstName,
+                middleName = s.middleName,
+                lastName = s.lastName,
+                dob = s.dob.ToString(),
+                startDate = s.startDate.ToString(),
+                profile = s.profile,
+                emailAddress = s.emailAddress,
+            };
+            return staffDTO;
+        }
+
+        public static IEnumerable<StaffDetailDTO> buildList(IEnumerable<Staff> allStaff)
+        {
+            var staff = allStaff.Select(s =>
+                       new Models.StaffDetailDTO()
+                       {
+                           staffCode = s.staffCode.Trim(),
+                           fullName = s.firstName + " " + (s.middleName == null ? "" : (s.middleName + " ")) + s.lastName,
+                           firstName = s.firstName,
+                           middleName = s.middleName,
+                           lastName = s.lastName,
+                           dob = s.dob.ToString(),
+                           startDate = s.startDate.ToString(),
+                           profile = s.profile,
+                           emailAddress = s.emailAddress,
+                       }).AsEnumerable();
+            return staff;
+        }
+
+    }
+}
