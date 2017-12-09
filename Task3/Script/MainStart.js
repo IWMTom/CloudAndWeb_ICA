8559fb92c02c846e0b14c02b6b1982ed3a55bb12
Tom Wilson <tom@iwm.pw>
2017-12-09 23:55:23 +0000

    Task 3 complete

--- Task3/Script/MainStart.js
+++ Task3/Script/MainStart.js
@@ -1,67 +1,76 @@
-﻿var http = new XMLHttpRequest();
+﻿var http = new XMLHttpRequest(); // Used to request data from a web server
 var coreUrl = "http://task2final.azurewebsites.net/api/";
 var buUrl = coreUrl + "BusinessUnit";
-var staffListUrl = coreUrl + "";
-var staffDetailUrl = coreUrl+ "";
+var staffListUrl = coreUrl + "Staff/BusinessUnit/";
+var staffDetailUrl = coreUrl+ "Staff/";
 
-
-function start() {
+function start()
+{
     hideStaffList();
-    http.onreadystatechange = getBuList;
-    http.open("GET", buUrl);
-    http.send();
+    http.onreadystatechange = getBuList; // Called whenever the status of the HTTP request changes
+    http.open("GET", buUrl); // Initialises a GET request to the Business Unit API
+    http.send(); // Sends the request to the server
 }
 
-function getBuList() {
-    if (http.readyState == 4 && http.status == 200) {
-        var buisinessUnits = JSON.parse(http.responseText);
-        if (buisinessUnits != null) {
-            displayItemsInBuList(buisinessUnits);
-        } else {
+function getBuList()
+{
+    if (http.readyState == 4 && http.status == 200) // Checks to see if the ready state is "done" and the HTTP status is 200 (OK)
+    {
+        var businessUnits = JSON.parse(http.responseText); // Parses the JSON response
+        if (businessUnits != null)
+        {
+            displayItemsInBuList(businessUnits);
+        }
+        else
+        {
             hideAll();
         }
     }
 }
 
-function displayItemsInBuList(arr) {
-          
-var table = document.getElementById("buList");
-
-table.innerHTML = "";
+function displayItemsInBuList(arr)
+{     
+    var table = document.getElementById("buList"); // Gets the HTML element with ID of "buList"
+    table.innerHTML = ""; // Empties the element
    
-    for (var i = 0; i < arr.length; i++) {     
+    for (var i = 0; i < arr.length; i++)
+    {     
         var row = table.insertRow(0);     
         var cell1 = row.insertCell(0);       
         var cell2 = row.insertCell(1);
         cell1.innerHTML = arr[i].title;       
         var id = arr[i].businessUnitCode;        
-        cell2.innerHTML = "&nbsp&nbsp&nbsp&nbsp<a href='#'     id='" + id + "' " + " >List Staff</a>";       
-        document.getElementById(id).onclick = getStaff;
+        cell2.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp; <a href='#' id='" + id + "' " + ">List Staff</a>";       
+        document.getElementById(id).onclick = getStaff; // Sets the 'onclick' function of the link to call getStaff()
     }
 }
 
-function getStaff(e) {
-
+function getStaff(e)
+{
     var detailUrl = staffListUrl + e.target.id;
     http.onreadystatechange = requestDetail;
-    http.open("GET", detailUrl);
+    http.open("GET", detailUrl); // Initialises a GET request to the Business Unit Detail API
     http.send();
-
 }
 
-function requestDetail() {
-    if (http.readyState == 4 && http.status == 200) {
+function requestDetail()
+{
+    if (http.readyState == 4 && http.status == 200)
+    {
         var staffList = JSON.parse(http.responseText);
-        if (staffList != null) {
+        if (staffList != null)
+        {
             displayStaffList(staffList);
-        } else {
+        }
+        else
+        {
             hideStaffList();
         }
     }
 }
 
-function displayStaffList(arr) {
-    
+function displayStaffList(arr)
+{ 
     document.getElementById("staffListHeader").style.visibility = "visible";
     document.getElementById("Staffdetail").style.visibility = "hidden";
     var table = document.getElementById("staffList");
@@ -69,43 +78,70 @@ function displayStaffList(arr) {
     if (arr != null)
     {
         table.innerHTML = "";
-        for (var i = 0; i < arr.length; i++) {   
+        for (var i = 0; i < arr.length; i++)
+        {   
             var row = table.insertRow(0);   
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             cell1.innerHTML = arr[i].fullName;
             var id = arr[i].staffCode;
-            cell2.innerHTML = "&nbsp&nbsp&nbsp&nbsp<a href='#'     id='" + id + "' " + " >Staff Detail</a>";
+            cell2.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp; <a href='#' id='" + id + "' " + ">Staff Detail</a>";
             document.getElementById(id).onclick = getStaffDetails;
         }
     }
 }
 
-function getStaffDetails(e) {
-  
+function getStaffDetails(e)
+{
+    var detailUrl = staffDetailUrl + e.target.id;
+    http.onreadystatechange = requestStaffDetail;
+    http.open("GET", detailUrl);
+    http.send(); 
 }
 
-function requestStaffDetail() {
-  
+function requestStaffDetail()
+{
+    if (http.readyState == 4 && http.status == 200) {
+        var staffDetail = JSON.parse(http.responseText);
+        if (staffDetail != null) {
+            displayStaffDetail(staffDetail);
+        }
+        else {
+            hideStaffDetail();
+        }
+    } 
 }
         
-function displayStaffDetail(staff) {   
+function displayStaffDetail(staff)
+{   
+    // This method formats the output of the staff detail and displays it in HTML
+
     document.getElementById("Staffdetail").style.visibility = "visible";
+    document.getElementById("staffCode").innerHTML = "Staff Code: " + staff.staffCode;
+    document.getElementById("staffFirstName").innerHTML = "First Name: " + staff.firstName;
+    document.getElementById("staffMiddleName").innerHTML = "Middle Name: " + staff.middleName;
+    document.getElementById("staffLastName").innerHTML = "Last Name: " + staff.lastName;
+    document.getElementById("staffEmail").innerHTML = "Email Address: " + staff.emailAddress;
+    document.getElementById("staffProfile").innerHTML = "Profile: " + staff.profile;
+    document.getElementById("staffStartDate").innerHTML = "Start Date: " + staff.startDate;
+    document.getElementById("staffDOB").innerHTML = "Date of Birth: " + staff.dob;
 }
 
-function hideAll() {
+function hideAll()
+{
     document.getElementById("buList").style.visibility = "hidden";
     hideStaffList();
 }
 
-function hideStaffList() {
+function hideStaffList()
+{
     document.getElementById("staffListHeader").style.visibility = "hidden";
     document.getElementById("staffList").style.visibility = "hidden";
-    document.getElementById("Staffdetail").style.visibility = "hidden";
-            
+    document.getElementById("Staffdetail").style.visibility = "hidden";         
 }
 
-function hideStaffDetail() {
+function hideStaffDetail()
+{
     document.getElementById("Staffdetail").style.visibility = "hidden";
 }
 
